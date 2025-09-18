52704cf64e71b83e1bf18ae2786abf842203dae5
klmckeig <kelly.l.mckeighan@intel.com>
2025-09-18 16:06:20 -0700

    update

--- km_bench_backup.sh
+++ km_bench_backup.sh
@@ -29,8 +29,12 @@ size_mb=$(echo "scale=2; $size_bytes/1024/1024" | bc)
 duration=$(echo "scale=2; $end - $start" | bc)
 throughput=$(echo "scale=4; $size_mb / $duration" | bc -l)
 
-headers="run_id,pg_ver,use_crc32c_avx512,duration(s),throughput(MB/s),scale_factor,backup_size(MB),start_time,end_time,output_log,pg_log,perf_log"
-values="$run_id,$pg_base,$USE_AVX512_CRC32C_BUILD,$duration,$throughput,$SCALE_FACTOR,$size_mb,$start,$end,$output_log,$pg_log_file,$perf_data"
+target_log_file=$pg_log_file
+search_target="pg_comp_crc32c_avx512"
+source km_parse_logs_for_checksum_len.sh
+
+headers="run_id,workload,pg_ver,duration(s),throughput(MB/s),scale_factor,backup_size(MB),start_time,end_time,server_cores,client_cores,crc32c_count,crc32c_len,output_log,pg_log,perf_log"
+values="$run_id,pg_basebackup,$pg_base,$duration,$throughput,$SCALE_FACTOR,$size_mb,$start,$end,$server_cores,$client_cores,$target_count,$target_avg,$output_log,$pg_log_file,$perf_data"
 
 echo "$headers"
 echo "$values"
---
+++ km_parse_logs_for_checksum_len.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+target_count=0
+target_avg=0
+nums=$(grep "LOG:  \[${search_target}\] len=" "$target_log_file" | sed -n "s/.*LOG:  \[${search_target}\] len=\([0-9]*\).*/\1/p")
+sum=0
+
+for n in $nums; do
+    sum=$((sum + n))
+    target_count=$((target_count + 1))
+done
+
+if [ "$target_count" -eq 0 ]; then
+    echo "No matching lines found."
+else
+    target_avg=$(echo "scale=2; $sum / $target_count" | bc)
+    echo "Average: $target_avg"
+fi
+
+echo target_count=$target_count
+echo target_avg=$target_avg
